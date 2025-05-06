b9984a53402f7a1950a3283b0ef5acefb911d3e1
Duke <DrDuky@gmail.com>
2025-05-06 09:53:44 -0400

    test

---
+++ sketch_may6a/sketch_may6a.ino
@@ -0,0 +1,11 @@
+#include "RTClib.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.println("hi");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
