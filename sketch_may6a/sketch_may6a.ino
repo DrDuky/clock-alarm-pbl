1fa02392ecc3790130a0b4828106a75a3df5acb7
Kaiser T. <kaiser.tayone@dawsoncollege.qc.ca>
2025-05-08 21:31:03 -0400

    Added Buzzer functions (tester)
    Remember to remove the static bool part for the buzzer, it's just used as a tester. I also added seconds  to the time.

--- sketch_may6a/sketch_may6a.ino
+++ sketch_may6a/sketch_may6a.ino
@@ -12,41 +12,84 @@ char keys[ROWS][COLS] = {
   { '1', '2', '3' },
   { '4', '5', '6' },
   { '7', '8', '9' },
-  { '#', '0', '*' }
+  { '*', '0', '#' }
 };
 
-byte rowPins[ROWS] = { 13, 6, 5, 4 };  // row pinouts of the keypad R1 = D8, R2 = D7, R3 = D6, R4 = D5
-byte colPins[COLS] = { 3, 2, A0 };      // column pinouts of the keypad C1 = D4, C2 = D3, C3 = D2
+byte rowPins[ROWS] = { 13, 6, 5, 4 };  // row pinouts of the keypad
+byte colPins[COLS] = { 3, 2, A0 };     // column pinouts of the keypad
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+const int buzzerPin = A1;     // Buzzer pin
+bool alarmTriggered = false;  // Keeps track if alarm is active
+
+void ringBuzzer() {  // defining function called ringBuzzer
+  digitalWrite(buzzerPin, HIGH);
+  delay(200);
+  digitalWrite(buzzerPin, LOW);
+  delay(200);
+  digitalWrite(buzzerPin, HIGH);
+}
+
+void ShowTime(int hour, int minute, int second) {
+  lcd.setCursor(0, 0);
+  lcd.print(" TIME: ");
+  if (hour < 10) lcd.print(" 0");
+  lcd.print(hour);
+  lcd.print(":");
+  if (minute < 10) lcd.print("0");
+  lcd.print(minute);
+  lcd.print(":");
+  if (second < 10) lcd.print("0");
+  lcd.print(second);
+}
 
 void setup() {
   lcd.begin(16, 2);
   Serial.begin(9600);
-  // put your setup code here, to run once:
+
+  pinMode(buzzerPin, OUTPUT);    // Set buzzer as output
+  digitalWrite(buzzerPin, LOW);  // Start with buzzer OFF
+
   if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
-    while (1)
-      ;
+    while (1);
   }
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));  // Set RTC time from computer
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  static bool tested = false;  // This keeps track if we already set the alarm once  (REMOVE THIS LATER, THIS IS JUST TO TEST BUZZER IN THE BEGINNING)
+  if (!tested) {
+    lcd.setCursor(0, 1);
+    lcd.print(" Stop alarm: #");
+    alarmTriggered = true;
+    tested = true;  // So it doesn’t repeat again
+  }
   DateTime now = rtc.now();
   int hour = now.hour();
   int minute = now.minute();
+  int seconds = now.second();
 
-  lcd.setCursor(0, 0);
-  lcd.print("TIME : ");
-  lcd.print(hour);
-  lcd.print(":");
-  lcd.print(now.minute());
+  ShowTime(now.hour(), now.minute(), now.second());
 
+  char key = keypad.getKey();
+  if (key != NO_KEY) {
+    Serial.println(key);
 
-  {
-    char key = keypad.getKey();
-    if (key != NO_KEY)
-      Serial.println(key);
+    // If alarm is triggered and user presses '#', stop the buzzer
+    if (alarmTriggered && key == '#') {
+      alarmTriggered = false;
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print(" Alarm  stopped");
+      delay(1000);  // show for 1 second
+      lcd.clear();
+    }
+  }
+  // Control the buzzer based on alarm state
+  if (alarmTriggered) {
+    ringBuzzer();
+  } else {
+    digitalWrite(buzzerPin, LOW);
   }
 }
